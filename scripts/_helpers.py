bc0f8fd3990d16f0da2a948d69df6d48c4565383
Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
2024-04-17 19:36:10 +0200

    Update README overview table automatically (#205)

--- scripts/_helpers.py
+++ scripts/_helpers.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
 
-benchmark_path = Path(__file__).parent.parent / 'Benchmark-Models'
+benchmark_path = Path(__file__).resolve().parent.parent / 'Benchmark-Models'
 petab_yamls = {
     petab_path.name: benchmark_path / petab_path.name / (petab_path.name + '.yaml')
     for petab_path in benchmark_path.glob('*')
     if petab_path.name != '.DS_Store'
 }
 petab_yamls = {k: petab_yamls[k] for k in sorted(petab_yamls)}
+
+readme_md = Path(__file__).resolve().parent.parent / "README.md"
